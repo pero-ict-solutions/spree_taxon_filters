aa83f4913c6b19868d957886bd8a530d61dab8c7
Peter Berkenbosch <peter@pero-ict.nl>
2013-04-04 21:25:55 +0200

    refactored the specs to improve readability

--- spec/models/spree/products_spec.rb
+++ spec/models/spree/products_spec.rb
@@ -3,43 +3,33 @@ require 'spec_helper'
 describe Spree::Product do
   let!(:taxonomy) { create(:taxonomy, :name => "Brand") }
 
-  it "response to by_* scope for the taxonomies" do
-    Spree::Taxonomy.first.filter_name.should eql :by_brand
-    Spree::Product.filter_by(taxonomy, "my brand").should be_empty
-  end
-
-  it "returns the products based on the taxon" do
-    product = create(:simple_product)
-    product.taxons << create(:taxon, :name => "my brand", :taxonomy => taxonomy)
-    Spree::Product.filter_by(taxonomy, "my brand").should include(product)
-  end
-
-  context "chained filters" do
-
-    it "filters all the products based on the chain" do
-      color_taxonomy = create(:taxonomy, :name => "Color")
-      red_taxon = create(:taxon, :name => "red", :taxonomy => color_taxonomy)
-      green_taxon = create(:taxon, :name => "green", :taxonomy => color_taxonomy)
-
-      product_red = create(:simple_product)
-      product_green = create(:simple_product)
-
-      sony_taxon = create(:taxon, :name => "sony", :taxonomy => taxonomy)
-
-      product_red.taxons << red_taxon
-      product_red.taxons << sony_taxon
-
-      product_green.taxons << green_taxon
-      product_green.taxons << sony_taxon
-
-      product_red.save
-      product_green.save
-
-      Spree::Product.filter_by(taxonomy, "sony").should include(product_red, product_green)
-      Spree::Product.filter_by(taxonomy, "sony").filter_by(color_taxonomy,"red").should include(product_red)
-      Spree::Product.filter_by(taxonomy, "sony").filter_by(color_taxonomy,"red").should_not include(product_green)
+  context "#filter_by" do
+    it "returns empty result when no taxon assigned" do
+      Spree::Product.filter_by(taxonomy, "sony").should be_empty
     end
 
-  end
+    context "when taxons are assigned to product" do
+      let(:taxonomy_by_color) {create(:taxonomy, :name => "Color") }
+      let(:red_taxon) { create(:taxon, :name => "red", :taxonomy => taxonomy_by_color) }
+      let(:green_taxon) { create(:taxon, :name => "green", :taxonomy => taxonomy_by_color) }
+      let(:sony_taxon) { create(:taxon, :name => "sony", :taxonomy => taxonomy) }
 
+      let(:product_red) { create(:simple_product, :taxons => [sony_taxon, red_taxon]) }
+      let(:product_green) { create(:simple_product, :taxons => [sony_taxon, green_taxon])}
+
+      it "returns the products that are assigned that taxon" do
+        Spree::Product.filter_by(taxonomy, "sony").should include(product_red, product_green)
+      end
+
+      context "and when chaining filter_by" do
+        it "include the products that match the extra filter" do
+          Spree::Product.filter_by(taxonomy, "sony").filter_by(taxonomy_by_color,"red").should include(product_red)
+        end
+
+        it "excludes the products that does not match the extra filter" do
+          Spree::Product.filter_by(taxonomy, "sony").filter_by(taxonomy_by_color,"red").should_not include(product_green)
+        end
+      end
+    end
+  end
 end
