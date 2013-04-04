e4757904be16e8b181c636fbcb74504c5bf947ec
Peter Berkenbosch <peter@pero-ict.nl>
2013-04-04 20:50:32 +0200

    use the 1.8 hash notation here as well.

--- spec/models/spree/taxonomy_spec.rb
+++ spec/models/spree/taxonomy_spec.rb
@@ -3,11 +3,11 @@ require "spec_helper"
 describe Spree::Taxonomy do
 
   it "create filter name by name" do
-    Spree::Taxonomy.create(name: "my test").filter_name.should eql :by_my_test
+    Spree::Taxonomy.create(:name => "my test").filter_name.should eql :by_my_test
   end
 
   it "singularize by the name" do
-    Spree::Taxonomy.create(name: "categories").filter_name.should eql :by_category
+    Spree::Taxonomy.create(:name => "categories").filter_name.should eql :by_category
   end
 
 end
