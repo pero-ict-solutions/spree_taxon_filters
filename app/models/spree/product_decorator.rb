170238cfab86cdf9431801fdc8f2775174930985
Peter Berkenbosch <peter@pero-ict.nl>
2013-04-15 21:42:12 +0200

    simplyfied, no more by name but by id.

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Product.class_eval do
-  def self.filter_by(taxonomy, name)
-    joins("INNER JOIN spree_products_taxons AS products_taxons_#{taxonomy.filter_name} ON (spree_products.id = products_taxons_#{taxonomy.filter_name}.product_id) INNER JOIN spree_taxons AS taxons_#{taxonomy.filter_name} ON (taxons_#{taxonomy.filter_name}.id = products_taxons_#{taxonomy.filter_name}.taxon_id)").where("taxons_#{taxonomy.filter_name}.name = ?", name)
+  def self.filter_by(taxonomy_id, taxon_id)
+    joins("INNER JOIN spree_products_taxons AS products_taxons_#{taxonomy_id} ON (spree_products.id = products_taxons_#{taxonomy_id}.product_id) INNER JOIN spree_taxons AS taxons_#{taxonomy_id} ON (taxons_#{taxonomy_id}.id = products_taxons_#{taxonomy_id}.taxon_id)").where("taxons_#{taxonomy_id}.id = ?", taxon_id)
   end
 end
--- lib/spree_taxon_filters/engine.rb
+++ lib/spree_taxon_filters/engine.rb
@@ -12,14 +12,9 @@ module SpreeTaxonFilters
     end
 
     def self.activate
-      taxonomy_decorator = File.join(File.dirname(__FILE__), '../../app/models/spree/taxonomy_decorator.rb')
-      product_decorator = File.join(File.dirname(__FILE__), '../../app/models/spree/product_decorator.rb')
-      Rails.configuration.cache_classes ? require(taxonomy_decorator) : load(taxonomy_decorator)
-      Rails.configuration.cache_classes ? require(product_decorator) : load(product_decorator)
-
-      # Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-      #   Rails.configuration.cache_classes ? require(c) : load(c)
-      # end
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
     end
 
     config.to_prepare &method(:activate).to_proc
