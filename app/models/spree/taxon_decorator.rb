53641614aba44867a551afc052bc36c251acfa9f
Peter Berkenbosch <peter@pero-ict.nl>
2013-04-16 13:12:04 +0200

    append the filters to params filters if present.

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -1,5 +1,15 @@
 Spree::Taxon.class_eval do
-  def to_filter_params
-    "filters[]=#{self.taxonomy_id},#{self.id}"
+  def to_filter_params(params = {})
+    filter_param = "filters[]=#{self.taxonomy_id},#{self.id}"
+    if params["filters"].present?
+      filter_params = []
+      filters = params["filters"]
+      filters.push("#{self.taxonomy_id},#{self.id}")
+      filters.each do |f|
+        filter_params.push "filters[]=#{f}"
+      end
+      filter_param = filter_params.join("&")
+    end
+    filter_param
   end
 end
--- spec/models/spree/taxons_spec.rb
+++ spec/models/spree/taxons_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Spree::Taxon do
-  let(:taxon) { create(:taxon)}
+  let(:taxon) { create(:taxon) }
 
   context "#to_filter_params" do
     it "returns the filters[] params for filtering" do
@@ -9,4 +9,27 @@ describe Spree::Taxon do
     end
   end
 
+  context "with another filter already in params" do
+
+    let(:other_taxon) { create(:taxon) }
+    let(:yet_other_taxon) { create(:taxon) }
+
+    context "when 1 filter present" do
+      let(:params) { {"filters" => ["#{other_taxon.taxonomy.id},#{other_taxon.id}"]}}
+      context "#to_filter_params" do
+        it "will append the taxon as a filter" do
+          taxon.to_filter_params(params).should == "filters[]=#{other_taxon.taxonomy.id},#{other_taxon.id}&filters[]=#{taxon.taxonomy.id},#{taxon.id}"
+        end
+      end
+    end
+
+    context "when more then 1 filters present" do
+      let(:params) { {"filters" => ["#{other_taxon.taxonomy.id},#{other_taxon.id}","#{yet_other_taxon.taxonomy.id},#{yet_other_taxon.id}"]}}
+      context "#to_filter_params" do
+        it "will append the taxon as a filter" do
+          taxon.to_filter_params(params).should == "filters[]=#{other_taxon.taxonomy.id},#{other_taxon.id}&filters[]=#{yet_other_taxon.taxonomy.id},#{yet_other_taxon.id}&filters[]=#{taxon.taxonomy.id},#{taxon.id}"
+        end
+      end
+    end
+  end
 end
